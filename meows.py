de9f416427ce1946fc39bf9dce4d3bab3a88981a
Senthil Loganathan <senthil.loganathan@gmail.com>
2026-03-15 17:55:24 +0800

    documentation in python

--- meows.py
+++ meows.py
@@ -1,2 +1,26 @@
-for i in range(3):
-    print("meow")
+# MEOWS = 3
+
+# for i in range(MEOWS):
+#     print("meow")
+
+# class Cat:
+#     MEOWS = 3
+
+#     def meow(self):
+#         for i in range(self.MEOWS):
+#             print("meow")
+    
+# cat = Cat()
+# cat.meow()
+
+def meow(n: int) -> None:
+    """ Prints n meows"""
+    for i in range(n):
+        print("meow")
+
+number = int(input("Number of meows: "))
+# number = input("Number of meows: ")
+# meow(number)
+
+meows: str = meow(number)
+print(meows)
